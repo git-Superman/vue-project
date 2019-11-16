ddbebed9b28dc88aaa34deabe6401345ebe40153
wqk <qiao_996@163.com>
2019-11-16 17:38:47 +0800

    first commit

---
+++ vue_demo/src/components/base.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <img src="@/assets/img/logo.png" alt="">
+    <div>
+        <h6>hello world</h6>
+    </div>
+</div>
+</template>
+<style scoped lang="less" >
+    div{
+        h6{
+            color:@color-red;
+            // font-size:@size-24;
+        }
+    }
+</style>
+<script>
+export default {
+    data(){
+        return {}
+    },
+    created(){
+        // this.axios.post('http://hh.gualaoshi.com/WxHome/GetCollectionListById?token=123').then(res=>{
+        //     console.log(res);
+        // });
+        this.$store.commit('increment',1000);
+
+        console.log(this.$store.state.count);
+    }
+}
+</script>
